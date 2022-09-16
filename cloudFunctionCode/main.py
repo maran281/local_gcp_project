616f2873b31b0a36c0e9534c0ce875ac7a1b5099
Manoj Rana (External - HCL Sweden AB) <manoj.rana@ingka.com>
2022-09-16 14:20:20 +0200

    checking7

--- cloudFunctionCode/main.py
+++ cloudFunctionCode/main.py
@@ -21,8 +21,7 @@ def hello_gcs(event, context):
     
     file = event
     fName = file['name']
-    timestamp1 = context.timestamp
-    print('Publish time is: '+ timestamp1)
+    print('Printing the blob creation timestamp :' + context.timestamp)
     if fName.endswith(".json"):
         function_start(fName, context, inTimeFormated)
         #print('processing file '+fName)
@@ -49,14 +48,10 @@ def function_start(fileName, context, inTime):
         topic_path = publisher_client.topic_path(project_id, topic_name)
         
         #Downloading the file to tmp location inside cloud function
-        # inboundblob = inbound_bucket.blob(fileName)
-        # inboundblobTime = inbound_bucket.time_created(fileName)
-        # print('printing the inbound blob publish time below')
-        # # print(inboundblob.time_created)
-        # print(inboundblobTime)
+        inboundblob = inbound_bucket.blob(fileName)
         destination_uri = '{}/{}'.format(folder,fileName)
         inboundblob.download_to_filename(destination_uri)
-        print("File has been downloaded inside cloud function at: "+destination_uri)
+        print("File has been downloaded inside clouf cuntion at: "+destination_uri)
 
         #Traversing the json file stored in tmp to publish
         #individual json documetns from the file to pubsub
